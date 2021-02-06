c6807bcc53cf5cfa50a34aea6713be970c57436e
rkumar <rkumar@agoda.com>
2021-02-06 18:09:32 +0700

    Add outlook

--- src/components/StockChecklist.jsx
+++ src/components/StockChecklist.jsx
@@ -2,6 +2,8 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 // v2.0
 // const generalStockFactors = ['peRatio', 'institutionalOwnership', 'insiderBuying', 'earnings', 'debtToEquityRatio', 'cashPosition'];
@@ -15,16 +17,20 @@ export const CheckList = function(props){
 }
 
 function getFactors(factors) {
-	return <Grid container>
-	      <List>
+	return <List>
 			{factors.map((f) => 
 				<ListItem>
-					<Grid item xs={10}>{f}</Grid>
+					<Grid item xs={5}>{f}</Grid>
 					<Grid item xs={2}>
 						<Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} /> 
+					</Grid>
+					<Grid item xs={5}>
+						<Select value={10}>
+							<MenuItem value={10}>Negative</MenuItem>
+	          				<MenuItem value={20}>Positive</MenuItem>
+						</Select>
 					</Grid>
 				</ListItem>
 			)}
 		</List>
-		</Grid>
 }
