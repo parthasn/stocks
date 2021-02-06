66ab538d2e18f291f5d50bb4869343cd18099621
rkumar <rkumar@agoda.com>
2021-02-06 18:12:03 +0700

    Add support for comments

--- src/components/StockChecklist.jsx
+++ src/components/StockChecklist.jsx
@@ -4,6 +4,8 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+
 
 // v2.0
 // const generalStockFactors = ['peRatio', 'institutionalOwnership', 'insiderBuying', 'earnings', 'debtToEquityRatio', 'cashPosition'];
@@ -20,15 +22,18 @@ function getFactors(factors) {
 	return <List>
 			{factors.map((f) => 
 				<ListItem>
-					<Grid item xs={5}>{f}</Grid>
-					<Grid item xs={2}>
+					<Grid item xs={4}>{f}</Grid>
+					<Grid item xs={1}>
 						<Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} /> 
 					</Grid>
-					<Grid item xs={5}>
+					<Grid item xs={3}>
 						<Select value={10}>
 							<MenuItem value={10}>Negative</MenuItem>
 	          				<MenuItem value={20}>Positive</MenuItem>
 						</Select>
+					</Grid>
+					<Grid item xs={4}>
+						<TextareaAutosize aria-label="empty textarea" placeholder="Comments" />
 					</Grid>
 				</ListItem>
 			)}
