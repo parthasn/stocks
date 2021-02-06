7b7a92d23482b4debeb66e2da5d9bfb6712a4f75
rkumar <rkumar@agoda.com>
2021-02-06 18:25:06 +0700

    Add field for stock name

--- src/components/StockChecklist.jsx
+++ src/components/StockChecklist.jsx
@@ -5,6 +5,8 @@ import Grid from '@material-ui/core/Grid';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Tooltip from '@material-ui/core/Tooltip';
+import TextField from '@material-ui/core/TextField';
 
 
 // v2.0
@@ -12,7 +14,8 @@ import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 export const CheckList = function(props){
 	const factors = getFactors(props.factors);
 	return <>
-			<h1>Checklist</h1>
+			<h3>Stock Analysis for: </h3>
+			<TextField id="standard-basic" label="Enter Stock Name" />
 			{factors}
 			</>;
 
@@ -22,7 +25,9 @@ function getFactors(factors) {
 	return <List>
 			{factors.map((f) => 
 				<ListItem>
-					<Grid item xs={4}>{f}</Grid>
+					<Tooltip title={f.description}>
+						<Grid item xs={4}>{f.factor}</Grid>
+					</Tooltip>
 					<Grid item xs={1}>
 						<Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} /> 
 					</Grid>
--- src/db/data.js
+++ src/db/data.js
@@ -1,3 +1,28 @@
 export var data = {
-	"generalStockFactors": ["peRatio", "institutionalOwnership", "insiderBuying", "earnings", "debtToEquityRatio", "cashPosition"]
+  "generalStockFactors": [
+    {
+      "factor": "P E Ratio",
+      "description": "Lower the better" 
+    },
+    {
+      "factor": "Institutional Ownership", 
+      "description": "Lower the better"
+    },
+    {
+      "factor": "Insider Buying", 
+      "description": "Positive if insiders/company is buying"
+    },
+    {
+      "factor": "Earnings", 
+      "description": "Check if they are sporadic or consistent"
+    },
+    {
+      "factor": "Debt To Equity Ratio", 
+      "description": ""
+    },
+    {
+      "factor": "Cash Position",
+      "description": ""
+    }
+  ]
 }
