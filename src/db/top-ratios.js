452b8d4019f8381c4d7e4ab38332f5ad44f7b704
rkumar <rkumar@agoda.com>
2021-07-11 18:54:04 +0700

    Clean comments

--- src/db/top-ratios.js
+++ src/db/top-ratios.js
@@ -7,11 +7,9 @@ function getItcPrice() {
     return (axios.get('https://www.screener.in/company/ITC/consolidated/'))
     .then((response) => {
         const html = cheerio.load(response.data);
-        // const topRatios = html('#top-ratios');
         const ratios = {}
         html('li', '#top-ratios').each((i, el) => {
           const stringValues = html(el).text().split('\n').filter(x => x.trim() !== '')
-          // print(stringValues)
           const key = stringValues.slice(0,1).join('').trim()
           const value = stringValues.slice(1, stringValues.length)
           ratios[key] = value
@@ -22,5 +20,3 @@ function getItcPrice() {
   }
 
 getItcPrice().then((b) => print(b))
-
-// main()
