b1abe8a4c29e86db8ae4ee90edf04e5262655cf4
Sunandan Bose <sunandan.riders@gmail.com>
2021-07-31 15:53:13 +0530

    Force destroy S3

--- deployment/S3_UI.tf
+++ deployment/S3_UI.tf
@@ -2,6 +2,7 @@ resource "aws_s3_bucket" "stock-ui-bucket" {
   bucket = "stock-ui-bucket"
   acl    = "public-read"  
   policy = file("./ui/policy.json")
+  force_destroy = true
 
   website {
     index_document = "index.html"
