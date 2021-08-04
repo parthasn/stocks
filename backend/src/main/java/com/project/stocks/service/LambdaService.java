72d12034093b10ec6b03e8457bfe3593ff947972
rkumar <rkumar@agoda.com>
2021-08-04 22:47:25 +0700

    Add AWS dependency

--- backend/src/main/java/com/project/stocks/service/LambdaService.java
+++ backend/src/main/java/com/project/stocks/service/LambdaService.java
@@ -1,9 +1,11 @@
 package com.project.stocks.service;
 
 import org.springframework.stereotype.Service;
+import com.amazonaws.services.lambda.invoke.LambdaFunction;
 
 @Service
 public class LambdaService {
+    @LambdaFunction(functionName="generateStockInfo")
     public void generateStockInformation(String stockId) {
 
     }
