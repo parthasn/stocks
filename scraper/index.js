3c641f0eab83f2e05aa13c93cb8b14c2fea42e96
rkumar <rkumar@agoda.com>
2021-08-07 00:17:58 +0700

    Transform data into json

--- scraper/index.js
+++ scraper/index.js
@@ -4,25 +4,30 @@ const axios = require('axios');
 const print = console.log
 
 var testHandler = exports.handler =  async function(event, context) {
-    print("EVENT: \n" + JSON.stringify(event, null, 2))
-    await getItcPrice().then((b) => print(b))
-    return context.logStreamName    
+    print(await getStockDetails(event))
+    return context.logStreamName
 }
 
-function getItcPrice() {
-    return (axios.get('https://www.screener.in/company/ITC/consolidated/'))
+function getStockDetails(stockId) {
+    return (axios.get(`https://www.screener.in/company/${stockId}/consolidated/`))
     .then((response) => {
         const html = cheerio.load(response.data);
-        const ratios = {}
+
+        const rawRatios = []
         html('li', '#top-ratios').each((i, el) => {
           const stringValues = html(el).text().split('\n').filter(x => x.trim() !== '')
           const key = stringValues.slice(0,1).join('').trim()
           const value = stringValues.slice(1, stringValues.length)
-          ratios[key] = value
+          rawRatios.push(value)
         })
-        
+
+        ratios = {}
+        const getMarketCap = () => rawRatios[0][1].trim()
+        const getPe = () => rawRatios[3][0].trim()
+        ratios['marketCap'] = getMarketCap()
+        ratios['pe'] = getPe()
         return ratios
       })
   }
 
-testHandler("Infy")
+testHandler("ITC")
