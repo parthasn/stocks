445b08945813b975ca92ef6e46dc2eaad412f941
Sunandan Bose <sunandan.riders@gmail.com>
2021-12-06 10:21:23 +0530

    Change PE calculation unit from Double to Integer

--- backend/src/test/java/com/project/stocks/service/ScoreServiceTest.java
+++ backend/src/test/java/com/project/stocks/service/ScoreServiceTest.java
@@ -15,35 +15,35 @@ class ScoreBuilderTest {
 
     @Test
     void givenPERatio39ScoreShouldBe4() {
-        Score score = new ScoreBuilder().withPE(39.0).build();
+        Score score = new ScoreBuilder().withPE(39).build();
 
         assertEquals(4, score.getValue());
     }
 
     @Test
     void givenPERatio1ScoreShouldBe5() {
-        Score score = new ScoreBuilder().withPE(1.0).build();
+        Score score = new ScoreBuilder().withPE(1).build();
 
         assertEquals(5, score.getValue());
     }
 
     @Test
     void givenPERatio41ScoreShouldBe3() {
-        Score score = new ScoreBuilder().withPE(41.0).build();
+        Score score = new ScoreBuilder().withPE(41).build();
 
         assertEquals(3, score.getValue());
     }
 
     @Test
     void givenPERatio61ScoreShouldBe2() {
-        Score score = new ScoreBuilder().withPE(61.0).build();
+        Score score = new ScoreBuilder().withPE(61).build();
 
         assertEquals(2, score.getValue());
     }
 
     @Test
     void givenPERatio81ScoreShouldBe1() {
-        Score score = new ScoreBuilder().withPE(81.0).build();
+        Score score = new ScoreBuilder().withPE(81).build();
 
         assertEquals(1, score.getValue());
     }
