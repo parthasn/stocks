8e4835f2882f7589b3a9e113db59c7c7033cdb0c
Sunandan Bose <sunandan.riders@gmail.com>
2021-12-07 18:21:08 +0530

    Change String with enum

--- backend/src/main/java/com/project/stocks/service/ScoreBuilder.java
+++ backend/src/main/java/com/project/stocks/service/ScoreBuilder.java
@@ -1,6 +1,7 @@
 package com.project.stocks.service;
 
 
+import com.project.stocks.dto.Logic;
 import com.project.stocks.dto.YearInfo;
 import com.project.stocks.model.Score;
 import org.springframework.stereotype.Service;
@@ -8,13 +9,13 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.project.stocks.dto.Logic.Decreasing;
+
 @Service
 public class ScoreBuilder {
 
-    private final String Increasing = "Increasing";
-    private final String Decreasing = "Decreasing";
-
     private Score score = new Score();
+    
     public ScoreBuilder withPE(Integer pe) {
         if(pe >= 1 && pe <=20)
             score.addValue(5);
@@ -35,17 +36,17 @@ public class ScoreBuilder {
 
 
     public ScoreBuilder withOPM(List<YearInfo> opmList) {
-        score.addValue(calculateYearlyStatistics(opmList, Increasing));
+        score.addValue(calculateYearlyStatistics(opmList, Logic.Increasing));
         return this;
     }
 
     public ScoreBuilder withNPM(List<YearInfo> npmList) {
-        score.addValue(calculateYearlyStatistics(npmList, Increasing));
+        score.addValue(calculateYearlyStatistics(npmList, Logic.Increasing));
         return this;
     }
 
     public ScoreBuilder withRevenue(List<YearInfo> revenueDetails) {
-        score.addValue(calculateYearlyStatistics(revenueDetails, Increasing));
+        score.addValue(calculateYearlyStatistics(revenueDetails, Logic.Increasing));
         return this;
     }
 
@@ -65,19 +66,17 @@ public class ScoreBuilder {
     2.  If logic is "Decreasing" then the score will increase if present
         year value is less than previous year value.
     */
-    private int calculateYearlyStatistics(List<YearInfo> inputList, String logic) {
-        if(logic == null || (logic != null && logic.equals("")) || inputList == null)
-            return 0;
+    private int calculateYearlyStatistics(List<YearInfo> inputList, Logic logic) {
         List<YearInfo> last6YearsOPMList = getLast6YearsRecordInDescendingOrder(inputList);
         int score = 0;
         for(int i = 1; i < last6YearsOPMList.size(); i++){
             int presentYear = last6YearsOPMList.get(i-1).getValue();
             int previousYear = last6YearsOPMList.get(i).getValue();
-            if(logic.equalsIgnoreCase(Increasing)){
+            if(logic.equals(Logic.Increasing)){
                 if(presentYear > previousYear)
                     score++;
             }
-            else if(logic.equalsIgnoreCase(Decreasing)){
+            else if(logic.equals(Decreasing)){
                 if(presentYear < previousYear)
                     score++;
             }
